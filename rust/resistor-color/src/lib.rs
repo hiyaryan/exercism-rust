c3c0f5b701b374b86272501bdd676960d7731f62
Ryan Meneses <rmenese1@asu.edu>
2023-01-31 17:25:45 -0800

    Complete: Resistor Color.

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,28 +1,54 @@
-#[derive(Debug, PartialEq, Eq)]
+use std::fmt::Debug;
+use enum_iterator::{all, Sequence};
+use int_enum::IntEnum;
+
+#[repr(u32)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, IntEnum, Sequence, PartialOrd, Ord)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 6,
+    Brown = 1,
+    Green = 5,
+    Grey = 8,
+    Orange = 3,
+    Red = 2,
+    Violet = 7,
+    White = 9,
+    Yellow = 4,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> u32 {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    if value == ResistorColor::Black.int_value() {
+        "Black".to_string()
+    } else if value == ResistorColor::Blue.int_value() {
+        "Blue".to_string()
+    } else if value == ResistorColor::Brown.int_value() {
+        "Brown".to_string()
+    } else if value == ResistorColor::Green.int_value() {
+        "Green".to_string()
+    } else if value == ResistorColor::Grey.int_value() {
+        "Grey".to_string()
+    } else if value == ResistorColor::Orange.int_value() {
+        "Orange".to_string()
+    } else if value == ResistorColor::Red.int_value() {
+        "Red".to_string()
+    } else if value == ResistorColor::Violet.int_value() {
+        "Violet".to_string()
+    } else if value == ResistorColor::White.int_value() {
+        "White".to_string()
+    } else if value == ResistorColor::Yellow.int_value() {
+        "Yellow".to_string()
+    } else {
+        "value out of range".to_string()
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    let mut v: Vec<ResistorColor> = all::<ResistorColor>().collect();
+    v.sort();
+    v
 }
