50476c0b40f43c9b6ab52126bf01e19eeaa2ae27
Jake Selig <jselig@rigetti.com>
2023-02-03 12:44:15 -0700

    chore: add comments to assembly-line review

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -1,10 +1,17 @@
 pub fn production_rate_per_hour(speed: u8) -> f64 {
     let prod = (speed as f64) * 221_f64;
 
+    // Instead of making a `prod * multiplier` call in each branch,
+    // isolate only the differing behavior of each branch.
+    //
+    // Also, matching on a range implies the `contains` check.
     let multiplier = match speed {
         1..=4 => 1.00,
         5..=8 => 0.90,
         9..=10 => 0.77,
+
+        // Return early to distinct this is an "exception", even though
+        // letting the multiplier be `0.00` would have the same effect.
         _ => return 0.00,
     };
 
@@ -12,6 +19,8 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
 }
 
 pub fn working_items_per_minute(speed: u8) -> u32 {
-    let rate_per_minute = production_rate_per_hour(speed) / 60_f64;
+    let rate_per_minute = production_rate_per_hour(speed) / 60.0;
+
+    // defer casting until as late as possible to retain precision
     rate_per_minute as u32
 }
